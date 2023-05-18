dc667d115d6a5693519c0949aab8dbf2db11346d
yunsuhee <suheeyun97@gmail.com>
2023-05-18 15:34:24 +0900

    Fix:jwt 추가

--- backend/notify/src/main/java/com/wohaha/notify/repository/NotifyRepository.java
+++ backend/notify/src/main/java/com/wohaha/notify/repository/NotifyRepository.java
@@ -13,10 +13,10 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface NotifyRepository extends ReactiveMongoRepository<Notify, String> {
 
-  //알림 읽지 않았을때, 최신순으로 조회
+  //알림을 읽지 않았을때, 최신순
   @Tailable
-  @Query(value = "{ $and: [ { receiveUserSeq: ?0 }, { readState: { $eq: false } } ] }", sort = "{ createdAt: -1 }")
-  Flux<Notify> findByReceiveUserSeq(Long receiveUserSeq);
+  @Query("{ $and: [ { receiveUserSeq: ?0 }, { readState: { $eq: false } } ] }")
+  Flux<Notify> findByReceiveUserSeq (Long receiveUserSeq);
 
 
 }
